c7bf24c126ff2bffaaef2739160f38b638a7eb4b
susameca <susamecaood@gmail.com>
2025-09-03 20:46:01 +0300

    Add initial plugin files

---
+++ to-do-plugin.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name:  To-do list
+Description:  To-do list plugin with admin page and Gutenberg block
+Author:       Tihomir Parushev
+Version:      1.0.0
+Author URI:   https://wordpress.org/plugins/bulgarisation-for-woocommerce/
+Requires PHP: 8.2
+Text Domain:  to-do
+License:      GPLv3 or later
+*/
+
+defined( 'ABSPATH' ) || exit;
+
+// Start the plugin
+add_action( 'plugins_loaded', 'to_do_plugin_init', 1, 0 );
+
+/**
+ * @return \To_Do\Plugin
+ */
+function to_do_plugin_init() {
+	load_plugin_textdomain( 'to-do', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+
+	if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+		require_once __DIR__ . '/vendor/autoload.php';
+	} else {
+		add_action( 'admin_notices', function() {
+			$message = sprintf( esc_html__( 'Please run `composer install` in the plugin folder `to-do-plugin`', 'to-do' ) );
+			echo wp_kses_post( sprintf( '<div class="error">%s</div>', wpautop( $message ) ) );
+		} );
+		
+		return;
+	}
+}
