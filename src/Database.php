ea529084caefc6e8e0ba218d67eb0d2591de9c5c
susameca <susamecaood@gmail.com>
2025-09-04 22:29:04 +0300

    Database: Add method to get all records

--- src/Database.php
+++ src/Database.php
@@ -15,7 +15,7 @@ class Database {
 	public function get_table_name() {
 		return $this->table_name;
 	}
-	
+
 	public function exists() {
 		$sql = $this->wpdb->prepare('SHOW TABLES LIKE %s', $this->table_name);
 		$found = $this->wpdb->get_var($sql);
@@ -53,16 +53,47 @@ class Database {
 	}
 
 	public function insert( $data ) {
-        $this->wpdb->insert( $this->table_name, $data );
+		$this->wpdb->insert( $this->table_name, $data );
 
-        return $this->wpdb->insert_id;
-    }
+		return $this->wpdb->insert_id;
+	}
 
-    public function update( $id, $data ) {
-        return $this->wpdb->update( $this->table_name, $data, ['id' => $id], null, ['%d'] );
-    }
+	public function update( $id, $data ) {
+		return $this->wpdb->update( $this->table_name, $data, ['id' => $id], null, ['%d'] );
+	}
 
-    public function delete( $id ) {
-        return $this->wpdb->delete( $this->table_name, ['id' => $id], ['%d'] );
-    }
+	public function delete( $id ) {
+		return $this->wpdb->delete( $this->table_name, ['id' => $id], ['%d'] );
+	}
+
+	public function get_all( $args = [] )  {
+		$allowed_order_by = [ 'id', 'title', 'created_at', 'updated_at' ];
+		$order_by = isset( $args['order_by'] ) && in_array( $args['order_by'], $allowed_order_by ) ? $args['order_by'] : 'created_at';
+		$order = isset( $args['order'] ) ? strtoupper( $args['order'] ) : 'ASC';
+		$limit = isset( $args['limit'] ) ? max( 0, (int) $args['limit'] ) : 0;
+		$offset = isset( $args['offset'] ) ? max( 0, (int) $args['offset'] ) : 0;
+
+		$sql = "SELECT id, title, description, category, created_at, updated_at FROM {$this->table_name} ORDER BY {$order_by} {$order}";
+		$params = [];
+
+		if ( $limit > 0 ) {
+			$sql .= ' LIMIT %d';
+			$params[] = $limit;
+
+			if ( $offset > 0 ) {
+				$sql .= ' OFFSET %d';
+				$params[] = $offset;
+			}
+		} elseif ( $offset > 0 ) {
+			$sql .= ' LIMIT %d OFFSET %d';
+			$params[] = PHP_INT_MAX;
+			$params[] = $offset;
+		}
+
+		if ( $params ) {
+			$sql = $this->wpdb->prepare( $sql, $params );
+		}
+
+		return $this->wpdb->get_results( $sql, ARRAY_A );
+	}
 }
