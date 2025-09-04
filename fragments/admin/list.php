ad8b5e29e1c3409b60d67628ffcc6e2a369edf6d
susameca <susamecaood@gmail.com>
2025-09-04 20:59:04 +0300

    Add basic template

--- fragments/admin/list.php
+++ fragments/admin/list.php
@@ -1,1 +1,67 @@
-<h2>Hello World!</h2>
+<div class="wrap">
+	<h2 class="hndle ui-sortable-handle"><span>To-do List</span></h2>
+
+	<table class="wp-list-table widefat fixed striped posts">
+		<thead>
+			<tr>
+				<th> <strong> Title </strong> </th>
+				<th> <strong> Creation date </strong> </th>
+				<th> <strong> Last update date </strong> </th>
+				<th> <strong> Category </strong> </th>
+				<th> <strong> Actions </strong> </th>
+			</tr>
+		</thead>
+
+		<tbody id="the-list">
+			<tr>
+				<td> <strong> To-do #1 </strong> </td>
+				<td> 3/9/2025 </td>
+				<td> 4/9/2025 </td>
+				<td> Home </td>
+				<td> <a href="#">Edit</a> <a href="#">Delete</a> </td>
+			</tr>
+
+			<tr>
+				<td> <strong> To-do #2 </strong> </td>
+				<td> 3/9/2025 </td>
+				<td> 4/9/2025 </td>
+				<td> Home </td>
+				<td> <a href="#">Edit</a> <a href="#">Delete</a> </td>
+			</tr>
+
+			<tr>
+				<td> <strong> To-do #3 </strong> </td>
+				<td> 3/9/2025 </td>
+				<td> 4/9/2025 </td>
+				<td> Home </td>
+				<td> <a href="#">Edit</a> <a href="#">Delete</a> </td>
+			</tr>
+
+			<tr>
+				<td> <strong> To-do #4 </strong> </td>
+				<td> 3/9/2025 </td>
+				<td> 4/9/2025 </td>
+				<td> Home </td>
+				<td> <a href="#">Edit</a> <a href="#">Delete</a> </td>
+			</tr>
+
+			<tr>
+				<td> <strong> To-do #5 </strong> </td>
+				<td> 3/9/2025 </td>
+				<td> 4/9/2025 </td>
+				<td> Home </td>
+				<td> <a href="#">Edit</a> <a href="#">Delete</a> </td>
+			</tr>
+		</tbody>
+
+		<tfoot>
+			<tr>
+				<th> <strong> Title </strong> </th>
+				<th> <strong> Creation date </strong> </th>
+				<th> <strong> Last update date </strong> </th>
+				<th> <strong> Category </strong> </th>
+				<th> <strong> Actions </strong> </th>
+			</tr>
+		</tfoot>
+	</table>
+</div>
